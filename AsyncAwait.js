d857d9bce3aff3efab7fb646adf778b7f88fa356
rahimwebdeveloper <info.abdurrahim12@gmail.com>
2022-09-26 01:31:22 +0600

    asyns awit

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -8,8 +8,23 @@ const promise = new Promise((resolve, reject) => {
 
 async function get() {
     // const res = [] ;
-    const res = await promise ;
-    console.log(res) ;
+    const res = await promise;
+    console.log(res);
 }
 
 get();
+
+
+// fetch("").then(res => res.json()).then(data => console.log(data));
+
+async function getUser() {
+    // const res = await fetch("");
+    // const data = await res.json();
+
+    const res = await fetch(`https://www.example.com/${user.id}`);
+    const data2 = await res.json();
+    console.log( data2);
+}
+
+
+getUser();
